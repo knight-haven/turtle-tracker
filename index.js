3e47fcdac137d10fb6ae88b772c78de45e306582
Jonathan Ellis <jde27@students.calvin.edu>
2019-11-11 21:18:33 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const db = require('./queries')
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+app.get('/turtle', db.getTurtles)
+app.get('/turtle/:id', db.getTurtleById)
+app.post('/turtle', db.createTurtle)
+app.put('/turtle/:id', db.updateTurtle)
+app.delete('/turtle/:id', db.deleteTurtle)
+app.get('/sighting', db.getSightings)
+app.get('/sighting/:id', db.getSightingById)
+app.post('/sighting', db.createSighting)
+app.put('/sighting/:id', db.updateSighting)
+app.delete('/sighting/:id', db.deleteSighting)
+app.get('/sighting/turtle/:turtleId', db.getSightingByTurtleId)
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+})
---
+++ queries.js
@@ -0,0 +1,149 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'pgithsli',
+  host: 'salt.db.elephantsql.com',
+  database: 'pgithsli',
+  password: 'IyT1o_L7Uiz2Mfyt0a7vXwdkWVGNyYqH',
+  port: 5432,
+})
+
+const getTurtles = (request, response) => {
+    pool.query('SELECT * FROM turtle ORDER BY id', (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+
+  const getTurtleById = (request, response) => {
+    const id = parseInt(request.params.id)
+  
+    pool.query('SELECT * FROM turtle WHERE id = $1', [id], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+
+  const createTurtle = (request, response) => {
+    const { mark, sex } = request.body
+  
+    pool.query('INSERT INTO turtle (mark, sex) VALUES ($1, $2)', [mark, sex], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(201).send(`Turtle added with ID: ${results.insertId}`)
+    })
+  }
+
+  const updateTurtle = (request, response) => {
+    const id = parseInt(request.params.id)
+    const { mark, sex } = request.body
+  
+    pool.query(
+      'UPDATE turtle SET mark = $1, sex = $2 WHERE id = $3',
+      [firstName, lastName, id],
+      (error, results) => {
+        if (error) {
+          throw error
+        }
+        response.status(200).send(`Turtle modified with ID: ${id}`)
+      }
+    )
+  }
+
+  const deleteTurtle = (request, response) => {
+    const id = parseInt(request.params.id)
+  
+    pool.query('DELETE FROM turtle WHERE id = $1', [id], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`Turtle deleted with ID: ${id}`)
+    })
+  }
+
+  const getSightings = (request, response) => {
+    pool.query('SELECT * FROM sighting ORDER BY time_seen DESC', (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+
+  const getSightingById = (request, response) => {
+    const id = parseInt(request.params.id)
+  
+    pool.query('SELECT * FROM sighting WHERE id = $1', [id], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+
+  const createSighting = (request, response) => {
+    const { turtleId, time, location, latitude, longitude, length, notes } = request.body
+  
+    pool.query('INSERT INTO sighting (turtle_id, time_seen, turtle_location, latitude, longitude, carapace_length, notes) VALUES ($1, $2, $3, $4, $5, $6, $7)', [turtleId, time, location, latitude, longitude, length, notes], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(201).send(`Sighting added with ID: ${results.insertId}`)
+    })
+  }
+
+  const updateSighting = (request, response) => {
+    const id = parseInt(request.params.id)
+    const { turtleId, time, location, latitude, longitude, length, notes } = request.body
+  
+    pool.query(
+      'UPDATE turtle SET turtle_id = $1, time_seen = $2, turtle_location = $3, latitude = $4, longitude = $5, carapace_length = $6, notes = $7 WHERE id = $8',
+      [turtleId, time, location, latitude, longitude, length, notes, id ],
+      (error, results) => {
+        if (error) {
+          throw error
+        }
+        response.status(200).send(`Sighting modified with ID: ${id}`)
+      }
+    )
+  }
+
+  const deleteSighting = (request, response) => {
+    const id = parseInt(request.params.id)
+  
+    pool.query('DELETE FROM sighting WHERE id = $1', [id], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`Sighting deleted with ID: ${id}`)
+    })
+  }
+
+  const getSightingByTurtleId = (request, response) => {
+    const turtleId = parseInt(request.params.turtleId)
+  
+    pool.query('SELECT * FROM turtle, sighting WHERE turtle.id = turtle_id AND turtle_id = $1 ORDER BY time_seen DESC', [turtleId], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+
+  module.exports = {
+    getTurtles,
+    getTurtleById,
+    createTurtle,
+    updateTurtle,
+    deleteTurtle,
+    getSightings,
+    getSightingById,
+    createSighting,
+    updateSighting,
+    deleteSighting,
+    getSightingByTurtleId,
+  }

83f7917b252445592db2ade4650641e6bed5e1b7
Jonathan Ellis <jde27@students.calvin.edu>
2019-12-11 11:11:19 -0500

    Remove photo code from backend

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const multer = require('multer')
 const app = express()
 const db = require('./queries')
-fs = require('fs-extra')
-
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb://heroku_8wwclj27:rlbqdqs4aiht7jend70laecogs@ds253398.mlab.com:53398/heroku_8wwclj27"
-
-MongoClient.connect(uri, (err, client) => {
-  if (err) return console.log(err)
-  mongoDB = client.db('test') 
-})
 
 app.use(bodyParser.json())
 app.use(
@@ -19,17 +9,6 @@ app.use(
     extended: true,
   })
 )
-
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads')
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now())
-  }
-})
- 
-var upload = multer({ storage: storage })
 
 app.get('/', (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API' })
@@ -47,25 +26,6 @@ app.put('/sighting/:id', db.updateSighting)
 app.delete('/sighting/:id', db.deleteSighting)
 app.get('/sighting/turtle/:turtleId', db.getSightingByTurtleId)
 
-// https://code.tutsplus.com/tutorials/file-upload-with-multer-in-node--cms-32088
-app.post('/photo', upload.single('photo'), (req, res) => {
-	var img = fs.readFileSync(req.file.path);
-  var encode_image = img.toString('base64');
-  var finalImg = {
-      contentType: req.file.mimetype,
-      image:  new Buffer(encode_image, 'base64')
-   };
-  db.collection('mycollection').insertOne(finalImg, (err, result) => {
-  	console.log(result)
-
-    if (err) return console.log(err)
-
-    console.log('Saved to database!')
-    res.redirect('/')
-  
-    
-  })
-});
 
 let port = process.env.PORT;
 if (port == null || port == "") {

3172e7a4fd2524381ac6afee6bf0590102a94171
Charles Kornoelje <charleskornoelje@gmail.com>
2020-02-20 15:39:37 -0500

    Change SelectTurtleScreen to a FC.

--- frontend/Screens/SelectTurtleScreen.js
+++ frontend/Screens/SelectTurtleScreen.js
@@ -3,21 +3,19 @@ import { View } from 'react-native';
 import TurtleList from '../components/TurtleList';
 import HeaderButton from '../components/HeaderButton';
 
-export default class SelectTurtleScreen extends React.Component {
-    static navigationOptions = ({ navigation }) => ({
-        headerLeft: () => (
-            <HeaderButton navigation={navigation}/>
-        )
-    })
-
-    render() {
-        return (
-            <View style={{ justifyContent: 'center' }}>
-                <TurtleList
-                    navigation={this.props.navigation}
-                    onPressPage="SightingEdit"
-                />
-            </View>
-        );
-    }
+export default function SelectTurtleScreen( {navigation} ) {
+    return (
+        <View style={{ justifyContent: 'center' }}>
+            <TurtleList
+                navigation={navigation}
+                onPressPage="SightingEdit"
+            />
+        </View>
+    );
 }
+
+SelectTurtleScreen.navigationOptions = ({ navigation }) => ({
+    headerLeft: () => (
+        <HeaderButton navigation={navigation} />
+    )
+})

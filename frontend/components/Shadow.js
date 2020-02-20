c398e4f49abdbc8fb33fbc0e742d2815731e25bd
Charles Kornoelje <charleskornoelje@gmail.com>
2020-02-20 14:23:01 -0500

    Make global shadow style.

---
+++ frontend/components/Shadow.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+/* Global shadow style.
+*/
+
+export default StyleSheet.create({
+    shadow: {
+
+        // TODO? Maybe remove this UI feature.
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.23,
+        shadowRadius: 2.62,
+        elevation: 4,
+    }
+});

cc141c308fee0262ca3f9df2d66b69a7c78b625c
Charles Kornoelje <charleskornoelje@gmail.com>
2020-02-23 19:41:14 -0500

    Add comments to help me remember what to do.

--- frontend/components/HeaderButton.js
+++ frontend/components/HeaderButton.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import IconButton from './IconButton';
 
+// TODO: Add prop for left and right side of header
+// TODO: Add prop for onPress()
+
+/*
+    Implements IconButton specifically to fit on headers of pages.
+*/
+
 export default function HeaderButton(props) {
     const {
         navigation,

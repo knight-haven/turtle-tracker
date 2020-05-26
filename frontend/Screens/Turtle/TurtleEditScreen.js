5196bdb7dc14b50d76c9abedc3ed93aac644c1b8
Charles Kornoelje <charleskornoelje@gmail.com>
2020-05-25 20:39:37 -0400

    remove unused imports

--- frontend/Screens/Turtle/TurtleEditScreen.js
+++ frontend/Screens/Turtle/TurtleEditScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import RadioForm from 'react-native-simple-radio-button';
 import Screen from '../../components/Screen';
 import HeaderButton from '../../components/HeaderButton';
--- frontend/components/TurtleMapView.js
+++ frontend/components/TurtleMapView.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, Button, Image, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { View, Image } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 
 /*

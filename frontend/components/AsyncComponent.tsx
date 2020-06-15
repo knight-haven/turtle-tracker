9bf9ea3095764b182d12ea4dfd3474ce242939fb
Charles Kornoelje <charleskornoelje@gmail.com>
2020-06-14 20:07:57 -0400

    add AsyncComponent.tsx

---
+++ frontend/components/AsyncComponent.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+interface IAsyncComponent {
+  Component: JSX.Element;
+  isError: boolean;
+  isLoading: boolean;
+}
+
+export const AsyncComponent = (props: IAsyncComponent) => {
+  const { Component, isError, isLoading } = props;
+
+  return (
+    <>
+      {isError && <div>Something went wrong.</div>}
+      {isLoading ? <div>Loading...</div> : <>{Component}</>}
+    </>
+  );
+};

6501f7ad7cb9952b6fb7ddca52e00a3d83d4690d
Jonathan Ellis <jde27@students.calvin.edu>
2020-06-18 16:41:24 -0400

    Update TurtleCard

---
+++ frontend/components/BottomDivider.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default function BottomDivider() {
+    return (
+        <View
+            style={{
+                borderBottomWidth: 1,
+                borderColor: '#d3d3d3',
+                width: '100%',
+            }}
+        />
+    )
+}
--- frontend/components/TurtleCard.js
+++ frontend/components/TurtleCard.js
@@ -3,6 +3,7 @@ import { View, StyleSheet, Text } from 'react-native';
 import moment from 'moment';
 import TurtleText from './TurtleText';
 import Divider from './Divider';
+import BottomDivider from './BottomDivider';
 import s from './Styles';
 
 
@@ -11,19 +12,22 @@ export default function TurtleCard({turtle, originalDate, recentDate, recentLeng
     return (
         <View>
             <View style={[s.card, s.shadow]}>
-                <Text style={{ fontSize: 20, fontWeight: 'bold', textAlign: 'center' }}>Turtle #{turtle.turtle_number}</Text>
                 <View style={styles.rowContainer}>
+                    <TurtleText titleText='Number' baseText={turtle.turtle_number}/>
+                    <Divider/>
                     <TurtleText titleText='Mark' baseText={turtle.mark} />
+                </View>
+                <BottomDivider/>
+                <View style={styles.rowContainer}>
+                    <TurtleText titleText='Carapace Length' baseText={`${recentLength} mm`} />
                     <Divider/>
                     <TurtleText titleText='Sex' baseText={turtle.sex[0].toUpperCase() + turtle.sex.slice(1)} />
                 </View>
+                <BottomDivider/>
                 <View style={styles.rowContainer}>
                     <TurtleText titleText='Date Found' baseText={moment(originalDate).format('l')} />
                     <Divider/>
                     <TurtleText titleText='Date Last Seen' baseText={moment(recentDate).format('l')} />
-                </View>
-                <View style={styles.rowContainer}>
-                    <TurtleText titleText='Carapace Length' baseText={`${recentLength} mm`} />
                 </View>
             </View>
         </View>

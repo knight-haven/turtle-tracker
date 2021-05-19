32d2558705d51fa5eb8261cc8973698f92d4c952
Charles Kornoelje <33156025+charkour@users.noreply.github.com>
2021-05-19 10:27:17 -0400

    fix common screen bug

--- frontend/AppNavigator.js
+++ frontend/AppNavigator.js
@@ -20,176 +20,166 @@ import TurtleEditScreen from './Screens/Turtle/TurtleEditScreen';
 import TurtleViewScreen from './Screens/Turtle/TurtleViewScreen';
 import TurtleListScreen from './Screens/TurtleListScreen';
 
-const Stack = createStackNavigator();
-
-const CommonScreens = () => {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name='TurtleList'
-        component={TurtleListScreen}
-        options={({ navigation }) => ({
-          headerRight: () => (
-            <HeaderButton
-              name={'add-location'}
-              onPress={() => navigation.navigate('SelectTurtle')}
-            />
-          ),
-          headerLeft: () => (
-            <HeaderButton
-              name={'settings'}
-              onPress={() => navigation.navigate('Settings')}
-            />
-          ),
-        })}
-      />
-      <Stack.Screen
-        name='TurtleView'
-        component={TurtleViewScreen}
-        options={({ route, navigation }) => ({
-          title: route.turtle ? route.params.turtle.mark : '',
-          headerRight: () => (
-            <HeaderButton
-              onPress={() =>
-                navigation.navigate('TurtleEdit', {
-                  edit: 'true',
-                  turtle: route.params.turtle,
-                  originalDate: route.params.originalDate,
-                  recentDate: route.params.recentDate,
-                  recentLength: route.params.recentLength,
-                  refreshTurtleView: route.params.refreshTurtleView,
-                  refreshTurtleList: route.params.refreshTurtleList,
-                })
+// ref: https://github.com/react-navigation/react-navigation/issues/3790#issuecomment-688669597
+const getCommon = (Stack) => {
+  return [
+    <Stack.Screen
+      name='TurtleList'
+      component={TurtleListScreen}
+      options={({ navigation }) => ({
+        headerRight: () => (
+          <HeaderButton
+            name={'add-location'}
+            onPress={() => navigation.navigate('SelectTurtle')}
+          />
+        ),
+        headerLeft: () => (
+          <HeaderButton
+            name={'settings'}
+            onPress={() => navigation.navigate('Settings')}
+          />
+        ),
+      })}
+    />,
+    <Stack.Screen
+      name='TurtleView'
+      component={TurtleViewScreen}
+      options={({ route, navigation }) => ({
+        title: route.turtle ? route.params.turtle.mark : '',
+        headerRight: () => (
+          <HeaderButton
+            onPress={() =>
+              navigation.navigate('TurtleEdit', {
+                edit: 'true',
+                turtle: route.params.turtle,
+                originalDate: route.params.originalDate,
+                recentDate: route.params.recentDate,
+                recentLength: route.params.recentLength,
+                refreshTurtleView: route.params.refreshTurtleView,
+                refreshTurtleList: route.params.refreshTurtleList,
+              })
+            }
+            name={'edit'}
+          />
+        ),
+        headerLeft: () => (
+          <HeaderButton
+            onPress={() => navigation.goBack()}
+            name={'navigate-before'}
+          />
+        ),
+      })}
+    />,
+    <Stack.Screen
+      name='TurtleEdit'
+      component={TurtleEditScreen}
+      options={({ navigation }) => ({
+        title:
+          route.params.edit != undefined && route.params.edit
+            ? 'Edit Turtle'
+            : 'Add Turtle',
+        headerLeft: () => (
+          <HeaderButton
+            onPress={() => navigation.goBack()}
+            name={'navigate-before'}
+          />
+        ),
+      })}
+    />,
+    <Stack.Screen
+      name='SelectTurtle'
+      component={SelectTurtleScreen}
+      options={({ navigation }) => ({
+        headerLeft: () => (
+          <HeaderButton
+            onPress={() => navigation.goBack()}
+            name={'navigate-before'}
+          />
+        ),
+      })}
+    />,
+    <Stack.Screen
+      name='Settings'
+      component={SettingsScreen}
+      options={({ navigation }) => ({
+        headerLeft: () => (
+          <HeaderButton
+            onPress={() => navigation.goBack()}
+            name={'navigate-before'}
+          />
+        ),
+      })}
+    />,
+    <Stack.Screen
+      name='SightingView'
+      component={SightingViewScreen}
+      options={({ navigation }) => ({
+        title: 'Sighting',
+        headerRight: () => (
+          <HeaderButton
+            onPress={() =>
+              navigation.navigate('SightingEdit', {
+                sighting: route.params.sighting,
+                markerList: route.params.markerList,
+                turtleId: route.params.turtleId,
+                images: route.params.images,
+                refreshSightingView: route.params.refreshSightingView,
+                refreshTurtleView: route.params.refreshTurtleView,
+                edit: true,
+              })
+            }
+            name={'edit'}
+          />
+        ),
+        headerLeft: () => (
+          <HeaderButton
+            onPress={() => {
+              navigation.goBack();
+              if (navigation.state.params.refreshTurtleView != undefined) {
+                navigation.state.params.refreshTurtleView();
               }
-              name={'edit'}
-            />
-          ),
-          headerLeft: () => (
-            <HeaderButton
-              onPress={() => navigation.goBack()}
-              name={'navigate-before'}
-            />
-          ),
-        })}
-      />
-      <Stack.Screen
-        name='TurtleEdit'
-        component={TurtleEditScreen}
-        options={({ navigation }) => ({
-          title:
-            route.params.edit != undefined && route.params.edit
-              ? 'Edit Turtle'
-              : 'Add Turtle',
-          headerLeft: () => (
-            <HeaderButton
-              onPress={() => navigation.goBack()}
-              name={'navigate-before'}
-            />
-          ),
-        })}
-      />
-      <Stack.Screen
-        name='SelectTurtle'
-        component={SelectTurtleScreen}
-        options={({ navigation }) => ({
-          headerLeft: () => (
-            <HeaderButton
-              onPress={() => navigation.goBack()}
-              name={'navigate-before'}
-            />
-          ),
-        })}
-      />
-      <Stack.Screen
-        name='Settings'
-        component={SettingsScreen}
-        options={({ navigation }) => ({
-          headerLeft: () => (
-            <HeaderButton
-              onPress={() => navigation.goBack()}
-              name={'navigate-before'}
-            />
-          ),
-        })}
-      />
-      <Stack.Screen
-        name='SightingView'
-        component={SightingViewScreen}
-        options={({ navigation }) => ({
-          title: 'Sighting',
-          headerRight: () => (
-            <HeaderButton
-              onPress={() =>
-                navigation.navigate('SightingEdit', {
-                  sighting: route.params.sighting,
-                  markerList: route.params.markerList,
-                  turtleId: route.params.turtleId,
-                  images: route.params.images,
-                  refreshSightingView: route.params.refreshSightingView,
-                  refreshTurtleView: route.params.refreshTurtleView,
-                  edit: true,
-                })
-              }
-              name={'edit'}
-            />
-          ),
-          headerLeft: () => (
-            <HeaderButton
-              onPress={() => {
-                navigation.goBack();
-                if (navigation.state.params.refreshTurtleView != undefined) {
-                  navigation.state.params.refreshTurtleView();
-                }
-              }}
-              name={'navigate-before'}
-            />
-          ),
-        })}
-      />
-      <Stack.Screen
-        name='SightingEdit'
-        component={SightingEditScreen}
-        options={({ route, navigation }) => ({
-          title:
-            route.params.edit != undefined && route.params.edit
-              ? 'Edit Sighting'
-              : 'Add Sighting',
-          headerLeft: () => (
-            <HeaderButton
-              onPress={() => navigation.goBack()}
-              name={'navigate-before'}
-            />
-          ),
-        })}
-      />
-    </Stack.Navigator>
-  );
+            }}
+            name={'navigate-before'}
+          />
+        ),
+      })}
+    />,
+    <Stack.Screen
+      name='SightingEdit'
+      component={SightingEditScreen}
+      options={({ route, navigation }) => ({
+        title:
+          route.params.edit != undefined && route.params.edit
+            ? 'Edit Sighting'
+            : 'Add Sighting',
+        headerLeft: () => (
+          <HeaderButton
+            onPress={() => navigation.goBack()}
+            name={'navigate-before'}
+          />
+        ),
+      })}
+    />,
+  ];
 };
 
 // Stack of screens for the Map Tab.
 export const MapStack = () => {
+  const Stack = createStackNavigator();
+  const commonScreens = getCommon(Stack);
   return (
     <Stack.Navigator>
       <Stack.Screen name='Map' component={MapScreen} />
-      <Stack.Screen name='MapCommon' component={CommonScreens} />
+      {commonScreens}
     </Stack.Navigator>
   );
 };
 
 // Stacks of Screens for the Turtles Lab
 export const TurtleListStack = () => {
-  return (
-    <Stack.Navigator headerMode='none'>
-      <Stack.Screen name='TurtleListCommon' component={CommonScreens} />
-      {/* <Stack.Screen name='TurtleList' component={TurtleListScreen} />
-      <Stack.Screen name='TurtleView' component={TurtleViewScreen} />
-      <Stack.Screen name='TurtleEdit' component={TurtleEditScreen} />
-      <Stack.Screen name='SelectTurtle' component={SelectTurtleScreen} />
-      <Stack.Screen name='Settings' component={SettingsScreen} />
-      <Stack.Screen name='SightingView' component={SightingViewScreen} />
-      <Stack.Screen name='SightingEdit' component={SightingEditScreen} /> */}
-    </Stack.Navigator>
-  );
+  const Stack = createStackNavigator();
+  const commonScreens = getCommon(Stack);
+  return <Stack.Navigator headerMode='none'>{commonScreens}</Stack.Navigator>;
 };
 
 // Combine the two stacks together under their own tabs.
@@ -222,6 +212,7 @@ export const TabNav = () => {
 
 // Stack for the landing page and login.
 export const LandingNav = () => {
+  const Stack = createStackNavigator();
   return (
     <Stack.Navigator>
       <Stack.Screen name='Landing' component={LandingScreen} />
@@ -234,6 +225,7 @@ export const LandingNav = () => {
 // Combine all of the screens into one navigation
 const MainNavigator = () => {
   const [userSignedIn, setUserSignedIn] = React.useState(false);
+  const Stack = createStackNavigator();
 
   return (
     <AuthContext.Provider value={{ userSignedIn, setUserSignedIn }}>
